bff4b99f11db7360e02bf488fe83885099d7333f
jonathangomz <jonicgp97@gmail.com>
2021-04-07 11:02:38 -0500

    Improve search
    Add search by name and by id
    Add components for error and playlist
    Add style for error messages
    Improve state management

--- pages/cloner.js
+++ pages/cloner.js
@@ -1,29 +1,66 @@
 import Head from 'next/head'
-import { useRouter } from 'next/router'
 import styles from '../styles/Home.module.css'
 import cloner from '../styles/Cloner.module.css'
+import error from '../styles/Cloner.module.css'
 import cookie from 'cookie'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 export default function Cloner({ token }) {
+  const [searchResults, setSearchResults] = useState(undefined);
   const [playlist, setPlaylist] = useState(undefined);
-  const [playlistIdInput, setPlaylistIdInput] = useState('');
-  const [playlistId, setPlaylistId] = useState('');
+  const [searchText, setSearchText] = useState('');
+  const [error, setError] = useState(undefined);
 
-  useEffect(() => {
-    if(playlistId) {
-      fetch(`/api/playlists/${playlistId}`, {
+  function resetSearchState() {
+    setError(undefined);
+    setPlaylist(undefined);
+    setSearchResults(undefined);
+  }
+
+  function search() {
+    resetSearchState();
+
+    if(searchText) {
+      fetch('/api/search', {
+        method: 'POST',
+        body: JSON.stringify({ search_name: searchText }),
         headers: {
           Authorization: `${token.token_type} ${token.access_token}`,
           'Content-Type': 'application/json',
         }
       })
-      .then((res) => res.json())
-      .then((data) => setPlaylist(data));
+      .then((res) => res.ok ? res.json() : res)
+      .then((data) => {
+        if(data?.status && data?.status !== 200) {
+          setError(data);
+        }else {
+          setSearchResults(data);
+        }
+      })
+      .catch((err) => console.log(err));
     }
-  }, [playlistId]);
+  };
 
-  console.log(playlist);
+  function searchById() {
+    resetSearchState();
+
+    if(searchText) {
+      fetch(`/api/playlists/${searchText}`, {
+        headers: {
+          Authorization: `${token.token_type} ${token.access_token}`,
+        }
+      })
+      .then((res) => res.ok ? res.json() : res)
+      .then((data) => {
+        if(data?.status && data?.status !== 200) {
+          setError(data);
+        }else {
+          setPlaylist(data);
+        }
+      })
+      .catch((err) => console.log(err));
+    }
+  };
 
   return (
     <div className={styles.container}>
@@ -39,10 +76,15 @@ export default function Cloner({ token }) {
           Playlist cloner
         </h1>
 
-        <input className={cloner.search_bar} type="text" placeholder="playlist_id" onChange={(e) => setPlaylistIdInput(e.target.value)}/>
-        <button className={cloner.search_button} onClick={() => setPlaylistId(playlistIdInput)}>Search</button>
+        <input className={cloner.search_bar} type="text" placeholder="playlist_id or playlist_name" onChange={(e) => setSearchText(e.target.value)}/>
+        <div className={cloner.search_buttons_container}>
+          <button className={cloner.search_button} onClick={search}>Search by name</button>
+          <button className={cloner.search_button} onClick={searchById}>Search by id</button>
+        </div>
         
-        {playlist && (<img src={playlist.images[1].url} alt="Playlist mosaic" width={playlist.images[1].width}/>)}
+        {playlist && <Playlist images={playlist.images}/>}
+
+        {error && <Error status={error.status} />}
       </main>
 
       <footer className={styles.footer}>
@@ -55,6 +97,25 @@ export default function Cloner({ token }) {
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
+    </div>
+  );
+}
+
+function Error({ status }) {
+  return (
+    <div className={error.modal}>
+      {status === 404 && <p> Any playlist found with that Id </p>}
+      {status === 500 && <p> An error ocurrer </p>}
+    </div>
+  )
+}
+
+function Playlist({ images }) {
+  return (
+    <div>
+      {(Array.isArray(images) && images.length > 0) &&
+        (<img src={images[0].url} alt="Playlist image" width={300}/>)
+      }
     </div>
   );
 }
